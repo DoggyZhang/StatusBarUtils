c2f4dd10ee2679929227d5953441b0f5dbdd97ac
DoggyZhang <572924155@qq.com>
2017-01-03 14:09:37 +0800

    Initial commit

---
+++ app/src/main/java/com/statusbarutils/BaseActivity.java
@@ -0,0 +1,25 @@
+package com.statusbarutils;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.jaeger.statusbardemo.R;
+import com.library.StatusBarUtil;
+
+/**
+ * Created by Jaeger on 16/2/14.
+ * <p>
+ * Email: chjie.jaeger@gmail.com
+ * GitHub: https://github.com/laobie
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        setStatusBar();
+    }
+
+    protected void setStatusBar() {
+        StatusBarUtil.setColor(this, getResources().getColor(R.color.colorPrimary));
+    }
+}
